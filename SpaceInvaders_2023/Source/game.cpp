7fa0b4b5aa359f76f05217dd7f3e065f4e6ac7c5
raphaele3147 <quark265@connect.hku.hk>
2024-01-05 12:48:41 +0100

    Attempt to improve access to array iterators
    - using .at(i) instead of [i]
    - turning for-loops to ranged for-loops
    - will further improve Leaderboard in the future
    - will need to fix law of demeter via interfaces

--- SpaceInvaders_2023/Source/game.cpp
+++ SpaceInvaders_2023/Source/game.cpp
@@ -74,11 +74,11 @@ void Game::Update()
 
 		player.Update();
 
-		for (int i = 0; i < Aliens.size(); i++)
+		for (auto& alien : Aliens)
 		{
-			Aliens[i].Update();
+			alien.Update();
 
-			if (Aliens[i].position.y > GetScreenHeight() - player.player_base_height)
+			if (alien.position.y > GetScreenHeight() - player.player_base_height)
 			{
 				End();
 			}
@@ -100,49 +100,45 @@ void Game::Update()
 		background.Update(offset / 15);
 
 
-		for (int i = 0; i < Projectiles.size(); i++)
+		for (auto& projectile : Projectiles)
 		{
-			Projectiles[i].Update();
+			projectile.Update();
 		}
-		for (int i = 0; i < Walls.size(); i++)
+		for (auto& wall : Walls)
 		{
-			Walls[i].Update();
+			wall.Update();
 		}
 
-		for (int i = 0; i < Projectiles.size(); i++)
+		for (auto& projectile : Projectiles)
 		{
-			if (Projectiles[i].type == EntityType::PLAYER_PROJECTILE)
+			if (projectile.type == EntityType::PLAYER_PROJECTILE)
 			{
-				for (int a = 0; a < Aliens.size(); a++)
+				for (auto& alien : Aliens)
 				{
-					if (CheckCollision(Aliens[a].position, Aliens[a].radius, Projectiles[i].lineStart, Projectiles[i].lineEnd))
+					if (CheckCollision(alien.position, alien.radius, projectile.lineStart, projectile.lineEnd))
 					{
-						Projectiles[i].active = false;
-						Aliens[a].active = false;
+						projectile.active = false;
+						alien.active = false;
 						score += 100;
 					}
 				}
 			}
 
-			for (int i = 0; i < Projectiles.size(); i++)
+			if (projectile.type == EntityType::ENEMY_PROJECTILE)
 			{
-				if (Projectiles[i].type == EntityType::ENEMY_PROJECTILE)
+				if (CheckCollision({ player.x_pos, GetScreenHeight() - player.player_base_height }, player.radius, projectile.lineStart, projectile.lineEnd))
 				{
-					if (CheckCollision({ player.x_pos, GetScreenHeight() - player.player_base_height }, player.radius, Projectiles[i].lineStart, Projectiles[i].lineEnd))
-					{
-						Projectiles[i].active = false;
-						player.lives -= 1;
-					}
+					projectile.active = false;
+					player.lives -= 1;
 				}
 			}
 
-
-			for (int b = 0; b < Walls.size(); b++)
+			for (auto& wall : Walls)
 			{
-				if (CheckCollision(Walls[b].position, Walls[b].radius, Projectiles[i].lineStart, Projectiles[i].lineEnd))
+				if (CheckCollision(wall.position, wall.radius, projectile.lineStart, projectile.lineEnd))
 				{
-					Projectiles[i].active = false;
-					Walls[b].health -= 1;
+					projectile.active = false;
+					wall.health -= 1;
 				}
 			}
 		}
@@ -168,7 +164,7 @@ void Game::Update()
 			}
 
 			Projectile newProjectile;
-			newProjectile.position = Aliens[randomAlienIndex].position;
+			newProjectile.position = Aliens.at(randomAlienIndex).position;
 			newProjectile.position.y += 40;
 			newProjectile.speed = -15;
 			newProjectile.type = EntityType::ENEMY_PROJECTILE;
@@ -178,7 +174,7 @@ void Game::Update()
 
 		for (int i = 0; i < Projectiles.size(); i++)
 		{
-			if (Projectiles[i].active == false)
+			if (Projectiles.at(i).active == false)
 			{
 				Projectiles.erase(Projectiles.begin() + i);
 				i--;
@@ -186,7 +182,7 @@ void Game::Update()
 		}
 		for (int i = 0; i < Aliens.size(); i++)
 		{
-			if (Aliens[i].active == false)
+			if (Aliens.at(i).active == false)
 			{
 				Aliens.erase(Aliens.begin() + i);
 				i--;
@@ -194,7 +190,7 @@ void Game::Update()
 		}
 		for (int i = 0; i < Walls.size(); i++)
 		{
-			if (Walls[i].active == false)
+			if (Walls.at(i).active == false)
 			{
 				Walls.erase(Walls.begin() + i);
 				i--;
@@ -223,7 +219,7 @@ void Game::Update()
 					if ((key >= 32) && (key <= 125) && (letterCount < 9))
 					{
 						name[letterCount] = (char)key;
-						name[letterCount + 1] = '\0';
+						name[letterCount+1] = '\0';
 						letterCount++;
 					}
 
@@ -279,19 +275,19 @@ void Game::Render()
 
 		player.Render(resources.shipTextures[player.activeTexture]);
 
-		for (int i = 0; i < Projectiles.size(); i++)
+		for (auto& projectile : Projectiles)
 		{
-			Projectiles[i].Render(resources.laserTexture);
+			projectile.Render(resources.laserTexture);
 		}
 
-		for (int i = 0; i < Walls.size(); i++)
+		for (auto& wall : Walls)
 		{
-			Walls[i].Render(resources.barrierTexture);
+			wall.Render(resources.barrierTexture);
 		}
 
-		for (int i = 0; i < Aliens.size(); i++)
+		for (auto& alien : Aliens)
 		{
-			Aliens[i].Render(resources.alienTexture);
+			alien.Render(resources.alienTexture);
 		}
 		break;
 	case State::ENDSCREEN:
@@ -341,9 +337,9 @@ void Game::Render()
 
 			for (int i = 0; i < Leaderboard.size(); i++)
 			{
-				char* tempNameDisplay = Leaderboard[i].name.data();
+				char* tempNameDisplay = Leaderboard.at(i).name.data();
 				DrawText(tempNameDisplay, 50, 140 + (i * 40), 40, YELLOW);
-				DrawText(TextFormat("%i", Leaderboard[i].score), 350, 140 + (i * 40), 40, YELLOW);
+				DrawText(TextFormat("%i", Leaderboard.at(i).score), 350, 140 + (i * 40), 40, YELLOW);
 			}
 		}
 		break;
@@ -368,7 +364,7 @@ void Game::SpawnAliens()
 
 bool Game::CheckNewHighScore()
 {
-	if (score > Leaderboard[4].score)
+	if (score > Leaderboard.at(4).score)
 	{
 		return true;
 	}
@@ -383,7 +379,7 @@ void Game::InsertNewHighScore(std::string name)
 
 	for (int i = 0; i < Leaderboard.size(); i++)
 	{
-		if (newData.score > Leaderboard[i].score)
+		if (newData.score > Leaderboard.at(i).score)
 		{
 			Leaderboard.insert(Leaderboard.begin() + i, newData);
 
@@ -435,8 +431,6 @@ void Player::Update()
 		activeTexture++;
 		timer = 0;
 	}
-
-
 }
 
 void Player::Render(Texture2D texture)
@@ -543,16 +537,16 @@ void Background::Initialize(int starAmount)
 
 void Background::Update(float offset)
 {
-	for (int i = 0; i < Stars.size(); i++)
+	for (auto& star : Stars)
 	{
-		Stars[i].Update(offset);
+		star.Update(offset);
 	}
 }
 
 void Background::Render()
 {
-	for (int i = 0; i < Stars.size(); i++)
+	for (auto& star : Stars)
 	{
-		Stars[i].Render();
+		star.Render();
 	}
 }

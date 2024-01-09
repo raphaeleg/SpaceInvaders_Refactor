876214f0c5e44c18c211b91ec50c2d49912862b4
raphaele3147 <quark265@connect.hku.hk>
2024-01-09 13:10:41 +0100

    Update Leaderboard.cpp

--- SpaceInvaders_2023/Source/Leaderboard.cpp
+++ SpaceInvaders_2023/Source/Leaderboard.cpp
@@ -13,7 +13,7 @@ void Leaderboard::InsertNewHighScore(std::string name) {
 
 void Leaderboard::Render(int textPosX, int fontSize) noexcept {
 	for (const auto& player : records) {
-		const int posY = 140 + (&player - &records.at(0)) * 40;
+		const int posY = static_cast<int>(140 + (&player - &records.at(0)) * 40); //TODO: hard nope!
 		DrawText(player.name.data(), textPosX, posY, fontSize, YELLOW);
 		DrawText(TextFormat("%i", player.score), textPosX + 300, posY, fontSize, YELLOW);
 	}
